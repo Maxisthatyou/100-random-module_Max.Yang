c69b377bbd6046690ae98cd1ffb868a13f53ead8
Maxisthatyou <158555539+Maxisthatyou@users.noreply.github.com>
2024-06-18 16:57:04 -0700

    good

--- task2.py
+++ task2.py
@@ -6,13 +6,16 @@ def coin_flip():
         y = random.randint(1,2)
         if x == y:
             z = input("You were correct, would you like to play again? Yes(1) or No(2)> ")
-            if z != 1:
+            if z != "1":
                 break
         else:
             z = input("You were incorrect, would you like to play again? Yes(1) or No(2)> ")
-            if z != 1:
+            if z != "1":
                 break
     
         
 def main():
     coin_flip()
+    
+if __name__ == "__main__":
+    main()
