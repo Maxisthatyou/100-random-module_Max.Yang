3a7dba149e3e7d2396ac2f394f266744093d61f5
Maxisthatyou <158555539+Maxisthatyou@users.noreply.github.com>
2024-06-18 16:54:03 -0700

    06/18/2024

--- task2.py
+++ task2.py
@@ -1,19 +1,18 @@
 import random
-x = int(input("Heads(1) or tails(2)?> "))
-y = random.randint(1, 2)
 
-while x == y:
-    print(f"You guessed it!",end = ' ')
-    z = input("Would you like to play again> ")
-    y = random.randint(1, 2)
-    if z != "yes":
-        break
-    x = int(input("Heads(1) or tails(2)?> "))
-while x != y:
-    print("Sorry, you were incorrect!",end = " ")
-    z = input("Would you like to play again> ")
-    y = random.randint(1, 2)
-    if z != "yes":
-        break
-    x = int(input("Heads(1) or tails(2)?> "))
+def coin_flip():
+    for i in range(100000000000000000000):
+        x = int(input("Heads(1) or Tails(2)?> "))
+        y = random.randint(1,2)
+        if x == y:
+            z = input("You were correct, would you like to play again? Yes(1) or No(2)> ")
+            if z != 1:
+                break
+        else:
+            z = input("You were incorrect, would you like to play again? Yes(1) or No(2)> ")
+            if z != 1:
+                break
     
+        
+def main():
+    coin_flip()
--- task3.py
+++ task3.py
@@ -6,9 +6,9 @@ y = random.randint(1, 3)
 while x == 3:
     
     if y == 2:
-        print("The computer chose rock. You won!")
+        print("The computer chose paper. You won!")
     elif y == 1:
-        print("The computer chose rock. Tie!")
+        print("The computer chose scissors. Tie!")
     elif y == 3:
         print("The computer chose rock. You lost!")
     break
@@ -17,16 +17,16 @@ while x == 2:
     if y == 2:
         print("The computer chose paper. Tie!")
     elif y == 1:
-        print("The computer chose paper. You lost!")
+        print("The computer chose scissors. You lost!")
     elif y == 3:
-        print("The computer chose paper. You won!")
+        print("The computer chose rock. You won!")
     break
 
 while x == 1:
     if y == 2:
-        print("The computer chose scissors. You lost!")
+        print("The computer chose paper. You lost!")
     elif y == 1:
         print("The computer chose scissors. You won!")
     elif y == 3:
-        print("The computer chose scissors. Tie!")
+        print("The computer chose rock. Tie!")
     break
